6ec1618957fe6bc3989b0e43817a8471422d098d
mbiofficiali-cmyk <mbiofficiali@gmail.com>
2025-09-10 17:54:36 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,26 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+pluginManagement {
+    repositories {
+        // آینه‌ها برای پلاگین‌ها
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven-central.storage-download.googleapis.com/maven2")
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        // آینه‌ها برای وابستگی‌ها
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/jcenter") // برای بعضی قدیمی‌ها
+        maven("https://maven-central.storage-download.googleapis.com/maven2")
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "GraceVPN"
 include(":app")
