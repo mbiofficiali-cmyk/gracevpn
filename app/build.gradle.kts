58ab64eb3b88551220929de1d7b0869d8f3ebe44
mbiofficiali-cmyk <mbiofficiali@gmail.com>
2025-09-10 17:55:28 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.example.gracevpn"
     compileSdk = 34
+
     defaultConfig {
         applicationId = "com.example.gracevpn"
         minSdk = 24
@@ -12,13 +14,25 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-    buildFeatures { viewBinding = true }
-    buildTypes { release { isMinifyEnabled = false } }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguards-rules.pro"
+            )
+        }
+    }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity-ktx:1.9.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
 }
